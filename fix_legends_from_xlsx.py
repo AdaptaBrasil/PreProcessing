084fd45c6f4eba2f56caa6fab6cee59ea4b36c5f
Mário Carvalho <mariodearaujocarvalho@gmail.com>
2023-08-23 15:10:43 -0300

    Fix value pair in legends

--- fix_legends_from_xlsx.py
+++ fix_legends_from_xlsx.py
@@ -64,7 +64,13 @@ def main(args):
             df_local = pd.DataFrame(column_relation_data)
 
             # Create a list with 5 values between [minimum and maximum]
-            interval_min_max_list = generate_value_pairs(min_value, max_value, size_t)
+            # Verificar se o min_value e o max_value são None ou Nan
+            if pd.isnull(min_value) or pd.isnull(max_value):
+                print(f"min_value or max_value is null. New legend_id: {legend_id}")
+                # Criar interval_min_max_list com 5 valores [None, None]
+                interval_min_max_list = [[None, None]] * size_t
+            else: 
+                interval_min_max_list = generate_value_pairs(min_value, max_value, size_t)
             # bsucar a key no df_files_to_fix 'indicator_id'
             if debug:
                 print(f"Interval min max list: {interval_min_max_list}")
